8d452f2bca47a294de02d27ca422dba1070a4ed2
Óscar Mira <oscarmirap@gmail.com>
2021-09-02 11:54:41 +0200

    Update createdir.sh

--- createdir.sh
+++ createdir.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
-
+#I know this is too simple to be here. It is here as a reminder and statement.
+# I add this to all my scripts, because I'm tired of most programs used in genomics either failing to write results if output directory does not exist or the oposite, failing to overwrite if they do exist
 
 DIRECTORY="out/results"
 if [ -d "$DIRECTORY" ]
